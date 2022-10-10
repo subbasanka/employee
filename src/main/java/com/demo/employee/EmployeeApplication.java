cfc24175dc967594dace4dd6e946f9ab8c6e48f0
ssubbu07 <96999976+subbasanka@users.noreply.github.com>
2022-10-10 16:26:55 -0500

    Added Eureka Service Registry
    Configured  as client to the Eureka server

--- src/main/java/com/demo/employee/EmployeeApplication.java
+++ src/main/java/com/demo/employee/EmployeeApplication.java
@@ -7,9 +7,11 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableConfigServer
+@EnableEurekaClient
 public class EmployeeApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
